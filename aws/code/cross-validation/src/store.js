549ee5e40448ee8ff99c9075e3e23b946eb60894
Alexdalegend <alexcool12390@gmail.com>
2023-03-10 00:11:48 +0000

    remove styling2

--- aws/code/cross-validation/src/store.js
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store";
-
-export const margin = writable({ top: 5, bottom: 30, left: 5, right: 5 });
--- aws/code/train-test-validation/js/colors.js
+++
@@ -1,3 +0,0 @@
-export const trainColor = "#2D3142";
-export const testColor = "#FF934F";
-export const validationColor = "#058ED9";
